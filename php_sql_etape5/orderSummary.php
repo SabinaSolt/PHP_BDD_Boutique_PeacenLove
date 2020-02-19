00c83f6d4d71df01d002661c91a2bfb1f39d9fba
Sabina Soltobaeva <sabina.soltobaeva@gmail.com>
2020-02-19 10:18:14 +0100

    commite php_sql_etape5 fini et fonctionnel

--- php_sql_etape5/orderSummary.php
+++ php_sql_etape5/orderSummary.php
@@ -3,44 +3,48 @@ include('entete.php');
 
 echo '<div class="container p-3 my-3 border bg-dark text-white text-center font-weight-bold rounded"
         style="font-size:30px;">'. 'Votre commande a été passée. Le numéro de votre commande: '.$_SESSION['newIO'][0] .'</div>';
-var_dump($_SESSION);
+//var_dump($_SESSION);
 ?>
-
+<form  class ="form-horizontal formulaire " action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"
+       method ="POST" enctype="multipart/form-data">
 <div class="container p-3 my-3 border bg-dark text-white rounded ">
     <h2 class="form-label col-sm-12 text-center font-weight-bold">Récapitulatif de votre commande: </h2>
     <br>
-    <div class="row container p-3  my-3 bg-light text-dark rounded ">
+    <div class="row container p-3  my-3 bg-light text-dark rounded text-left ">
         <div class="col-sm-12 font-weight-bold">Information client:</div>
-        <div class="col-sm-12"> <?php echo $_SESSION['currentCustomer'][0].' '.$_SESSION['currentCustomer'][1]?></div><br>
+        <div class="col-sm-12 "> <?php echo $_SESSION['currentCustomer'][0].' '.$_SESSION['currentCustomer'][1]?></div><br>
         <div class ="col-sm-12  "><?php echo $_SESSION['currentCustomer'][2]?> </div><br>
         <div class ="col-sm-12 "><?php echo $_SESSION['currentCustomer'][3].' '.$_SESSION['currentCustomer'][4]?> </div>
 
     </div>
 
 
-    <form  class ="form-horizontal formulaire" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method ="POST" enctype="multipart/form-data">
         <?php
-        while($item = $catalog->fetch())
+        $requete5=$bdd->prepare("SELECT produit.nomProduit, produit.imageProduit, 
+                        produitcommande.quantiteCommande, produit.prix as 'prixUnit' 
+                        FROM `produitcommande` 
+                        INNER JOIN produit ON produitcommande.idProduit=produit.idProduit 
+                        WHERE `idCommande`=:numCommande");
+        $requete5->bindParam(':numCommande',$_SESSION['newIO'][0]);
+        $requete5->execute();
+        while($orderItem = $requete5->fetch())
         {?>
-            <div class="row container my-3 bg-light text-dark rounded ">
+            <div class="row container  my-3 bg-light text-dark rounded ">
 
-                <div class="col-sm-3"> <img class ="" src =" <?php echo $item['imageProduit'] ?> "alt="image"></div>
-                <div class ="col-sm-2 "><?php echo $item['nomProduit']?> </div>
-                <div class ="col-sm-4 item-desc "><?php echo $item['descriptionProduit']?> </div>
-                <div class ="col-sm-2 rounded-circle price "> <?php echo $item['prix'] ?> €</div>
-                <input class="form-control col-sm-1 " type="checkbox" name ="<?php echo 'checkbox'.$item['idProduit']?>">
-
+                <div class="col-sm-3"> <img class ="" src =" <?php echo $orderItem['imageProduit'] ?> "alt="image"></div>
+                <div class ="col-sm-2 "><?php echo $orderItem['nomProduit']?> </div>
+                <div class ="col-sm-2  "><?php echo $orderItem['quantiteCommande']?> </div>
+                <div class ="col-sm-2 "> <?php echo $orderItem['prixUnit'] ?> €</div>
+                <div class ="col-sm-2 "> <?php echo number_format($orderItem['prixUnit']*$orderItem['quantiteCommande'],2) ?> €</div>
             </div>
             <?php
+        }
 
-        } ?>
+        echo '<div class=" row sous-total container bg-light text-dark rounded col-sm-3 float-right" > Total: ',
+        totalPanier($bdd, $_SESSION['checkBoxes'], $_SESSION['quantite']), ' €</div>';
+        ?>
         <br>
-        <button class="btn btn-primary" type="submit" name="buttonSubmit"> Soumettre </button>
-
     </form>
 </div>
-
-
-
 </body>
 </html>
